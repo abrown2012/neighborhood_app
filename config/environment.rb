b644d66034af47e0eca53400f7ce8ba200736e1d
Agnieszka Brown <abrown2012@icloud.com>
2021-01-31 20:00:14 -0500

    add rakefile

--- config/environment.rb
+++ config/environment.rb
@@ -6,5 +6,5 @@ require_all 'app'
 
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
-  :database => "db/neighborhood#{ENV['SINATRA_ENV']}.sqlite"
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
