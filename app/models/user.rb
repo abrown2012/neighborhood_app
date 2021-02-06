50ad0583bdad974ce28f4af14e3711b9a8b165ac
Agnieszka Brown <abrown2012@icloud.com>
2021-02-06 17:46:40 -0500

    adds validations to user characteristics

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
     has_secure_password
+    has_many :posts
+    has_many :neighborhoods, through: user_neighborhoods
+    validates :email, presence: true, uniqueness: true 
+    validates :name, presence: true, length: {in: 5..30}, exclusion: {in: ["admin", "superadmin", "user", "name"] }
+    validates :password, presence: true, length: {in: 10..100}, confirmation: true, on: :create 
 end
