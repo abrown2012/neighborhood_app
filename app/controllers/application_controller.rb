d1532ea5c55603b7989bccb844ebaabdbe83af07
Agnieszka Brown <abrown2012@icloud.com>
2021-02-07 18:10:18 -0500

    adds logout page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,14 +10,7 @@ class ApplicationController < Sinatra::Base
     register Sinatra::Flash
     include Helpers 
 
-    get '/' do 
-        @users = User.all 
-        if is_logged?
-            erb:index 
-        else 
-            erb :index_log_in
-        end 
-    end 
+
 
     not_found do 
         erb :not_found 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
     get '/signup' do 
-            erb :'users/signup'
+            erb :'/users/signup'
     
     end 
 
@@ -16,7 +16,28 @@ class UsersController < ApplicationController
             redirect '/signup'
         end     
     end 
+    
+    get '/' do 
+        @users = User.all 
+        if is_logged?
+            erb :'/posts/feed'
+        else 
+            erb :index_log_in
+        end 
+    end 
 
+
+    post '/login' do 
+        user = User.find_by_email(params[:email])
+        if user && user.authenticate(params[:password])
+            session["user_id"] = user.id
+            flash[:success] = "Successfully logged in!"
+            redirect "/"
+          else
+            flash[:error] = "Invalid credentials"
+            redirect "/"
+          end
+    end 
 
 
 
