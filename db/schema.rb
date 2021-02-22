5d5592f3d785c785044c829347bbb96fef5e3ac2
Agnieszka Brown <abrown2012@icloud.com>
2021-02-21 22:58:49 -0500

    creates comment table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_21_201326) do
+ActiveRecord::Schema.define(version: 2021_02_22_035524) do
 
   create_table "neighborhoods", force: :cascade do |t|
     t.string "neighborhood_name"
